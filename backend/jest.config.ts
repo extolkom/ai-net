2b296549e8c88800fd543f7c89610ac308726f07
Ogunmodede Joel Taiwo <jotelfootball@gmail.com>
2026-06-17 16:56:07 +0100

    Network Statistics API with Aggregated Metrics and Time-Series Cache

---
+++ backend/jest.config.ts
@@ -0,0 +1,10 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/?(*.)+(spec|test).[tj]s']
+};
+
+export default config;
